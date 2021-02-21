e1f6b0f17327bfb844b29f9309067e28fc8cdc01
glebPC <ksendziuk.gleb@gmail.com>
2021-02-21 18:04:54 +0200

    Попытка вывести Html

--- blog/editor/views.py
+++ blog/editor/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def editor_index(request):
-    return HttpResponse('<html><body><h1>Hello! You are in app "EDITOR"</h1></body></html>')
+    app_name = 'editor'
+    template = loader.get_template('templates/base.html')
+    context = {
+        'app_name': app_name,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse('<html><body><h1>Hello! You are in app "EDITOR"</h1></body></html>') TODO
+
